d32caa1e4485ebe78c2449158b9b1b4c6c5b8f09
Ronak Agarwal <ronak.agarwal@sapient.com>
2020-06-28 14:23:05 +0100

    code

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/ronak-agarwal/terraform-provider-istio
+
+go 1.14
+
+require (
+	github.com/googleapis/gnostic v0.4.2 // indirect
+	github.com/hashicorp/terraform-plugin-sdk v1.14.0
+	github.com/imdario/mergo v0.3.9 // indirect
+	github.com/mitchellh/go-homedir v1.1.0
+	k8s.io/api v0.18.5 // indirect
+	k8s.io/client-go v0.18.5
+	k8s.io/utils v0.0.0-20200619165400-6e3d28b6ed19 // indirect
+)
--- istio/provider.go
+++ istio/provider.go
@@ -1,8 +1,15 @@
 package istio
 
 import (
+	"io/ioutil"
+
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/mitchellh/go-homedir"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 // Provider ...
@@ -28,4 +35,47 @@ func Provider() terraform.ResourceProvider {
 		},
 		ConfigureFunc: providerConfigure,
 	}
+}
+
+// Config ...
+type Config struct {
+	Client    dynamic.Interface
+	Clientset *kubernetes.Clientset
+}
+
+func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+	var data []byte
+	var config *rest.Config
+	var err error
+	path := d.Get("config_path").(string)
+	data, _ = readKubeconfigFile(path)
+	config, err = clientcmd.RESTConfigFromKubeConfig(data)
+	if err != nil {
+		// if neither worked we fall back to an empty default config
+		config = &rest.Config{}
+	}
+	client, err := dynamic.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+	return &Config{client, clientset}, nil
+}
+
+func readKubeconfigFile(s string) ([]byte, error) {
+	p, err := homedir.Expand(s)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := ioutil.ReadFile(p)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
 }
